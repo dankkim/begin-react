a5ec1fc51bb970b6eb0912d5c0fc28be8ebf8e85
Kim Younge Ae <jhy0088@naver.com>
2021-07-12 17:58:47 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from './Hello';
 
 function App() {
+  const style = {
+    backgroundColor: 'red',
+    color: 'aqua'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={style}>
+        aasd
+      </div>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </div>
   );
 }
