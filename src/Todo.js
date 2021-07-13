50ff3a563ca4a0b6990fb56c60bae38e5b84be45
Kim Young Ae <kya754@gmail.com>
2021-07-13 23:46:47 +0900

    todo

---
+++ src/Todo.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+
+
+function TodoList({todos, deleteTodo}) {
+
+  const list = todos.map((todo, index) => (
+    <li key={index}>
+      {todo}
+      <button onClick={() => deleteTodo(index)}>delete</button>
+    </li>
+  ))
+
+  return (
+    <ul className="list">
+      {list}
+    </ul>
+  );
+}
+
+
+
+function Todo() {
+
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handler = (e) => {
+    const {value} = e.target;
+    setText(value);
+  }
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      text
+    ])
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+    setText('');
+  }
+
+  const deleteTodo = (key) => {
+    setTodos(todos.filter((todo, index) => index !== key));
+  }
+
+  return (
+    <div className="wrap">
+      <h1>TODO!!</h1>
+      <div>
+        <input type="text" value={text} onChange={handler}/>
+        <button onClick={addTodo}>add</button>
+      </div>
+      <TodoList todos={todos} deleteTodo={deleteTodo}/>
+    </div>
+  )
+}
+
+export default Todo
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Counter from './Counter'
+import Todo from './Todo'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Todo/>
   </React.StrictMode>,
   document.getElementById('root')
 );
